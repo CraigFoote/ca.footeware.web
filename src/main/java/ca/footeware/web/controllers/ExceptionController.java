40b3d2416a9c919323bd37afbb58246b1c8d8c9d
craig <craig@notebook>
2022-11-30 09:04:12 -0500

    Beef up error page.

--- src/main/java/ca/footeware/web/controllers/ExceptionController.java
+++ src/main/java/ca/footeware/web/controllers/ExceptionController.java
@@ -1,19 +1,42 @@
 package ca.footeware.web.controllers;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 class ExceptionController {
 
-	@ExceptionHandler(value = Exception.class)
-	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) {
-		var mav = new ModelAndView();
-		mav.addObject("error", e.getMessage());
-		mav.setViewName("error");
-		return mav;
+	public static final String ERROR = "error";
+
+	@ExceptionHandler(value = Throwable.class)
+	public String defaultErrorHandler(HttpServletRequest req, Throwable t, Model model) {
+		model.addAttribute("timestamp", LocalDateTime.now());
+		model.addAttribute("path", req.getRequestURL());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		model.addAttribute(ERROR, t.getCause());
+		model.addAttribute("message", t.getMessage());
+		model.addAttribute("trace", extractTraces(t.getStackTrace()));
+		return ERROR;
+	}
+
+	/**
+	 * Creates a "\n"-delimited string from stacktrace.
+	 *
+	 * @param stackTrace {@link StackTraceElement}
+	 * @return {@link String}
+	 */
+	private String extractTraces(StackTraceElement[] stackTrace) {
+		StringBuilder buffer = new StringBuilder();
+		for (StackTraceElement stackTraceElement : stackTrace) {
+			buffer.append(stackTraceElement.toString());
+			buffer.append("\n");
+		}
+		return buffer.toString();
 	}
 }
